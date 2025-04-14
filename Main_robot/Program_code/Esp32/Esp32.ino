f9e7e5c0411ac4192d34db5d30a07bc9db3e59dc
Vova <olbar1012@gmail.com>
2025-04-14 20:59:48 +0300

    program for main robot updated, the ESP logic has been written

--- Main_robot/Program_code/Esp32/Esp32.ino
+++ Main_robot/Program_code/Esp32/Esp32.ino
@@ -1,3 +1,9 @@
+int sign(int x){
+  if (x > 0) return 1;
+  else if (x < 0) return -1;
+  else return 0;
+}
+
 #include <PS4Controller.h>
 
 unsigned long lastTimeStamp = 0;
@@ -19,22 +25,44 @@ void setup()
   Serial.println("Ready.");
 }
 
-int x, y;
+float eng1_F, eng2_F, eng3_F, x4, y4, x5, y5, x6, y6,
+L = 109.985226, 
+axis_x1_1 = 109.985226, axis_y1_1 = -63.5, axis_x2_1 = -109.985226, axis_y2_1 = 63.5,
+axis_x1_2 = -109.985226, axis_y1_2 = -63.5, axis_x2_2 = 109.985226, axis_y2_2 = 63.5,
+axis_x1_3 = 0, axis_y1_3 = 127, axis_x2_3 = 0, axis_y2_3 = -127,
+stic_x, stic_y,
+eng_x_1 = -54.992613, eng_y_1 = 31.75, eng_x_2 = 54.992613, eng_y_2 = 31.75, eng_x_3 = 0, eng_y_3 = -63.5,
+sec_wheels_x1_1 = -109.985226, sec_wheels_y1_1 = -63.5, sec_wheels_x2_1 = 0, sec_wheels_y2_1 = 127,
+sec_wheels_x1_2 = 109.985226, sec_wheels_y1_2 = -63.5, sec_wheels_x2_2 = 0, sec_wheels_y2_2 = 127,
+sec_wheels_x1_3 = -109.985226, sec_wheels_y1_3 = -63.5, sec_wheels_x2_3 = 109.985226, sec_wheels_y2_3 = -63.5;
+
 void loop() 
 {
   if (millis() - lastTimeStamp > 50)
   {
     lastTimeStamp = millis();
   }
-  x = PS4.LStickX();
-  y = PS4.LStickY();
+  stic_x = PS4.LStickX();
+  stic_y = PS4.LStickY();
 
-  if(abs(x) <= 10) x = 0;
-  if(abs(y) <= 10) y = 0;
+  if(abs(stic_x) <= 10) stic_x = 0;
+  if(abs(stic_y) <= 10) stic_y = 0;
 
-  x = map(x, -127, 127, -255, 255);
-  y = map(y, -127, 127, -255, 255);
+  x4=((sec_wheels_x2_1-sec_wheels_x1_1)*(sec_wheels_y2_1-sec_wheels_y1_1)*(stic_y-sec_wheels_y1_1)+sec_wheels_x1_1*pow(sec_wheels_y2_1-sec_wheels_y1_1, 2)+stic_x*pow(sec_wheels_x2_1-sec_wheels_x1_1, 2))/(pow(sec_wheels_y2_1-sec_wheels_y1_1, 2)+pow(sec_wheels_x2_1-sec_wheels_x1_1, 2));
+  y4=(sec_wheels_y2_1-sec_wheels_y1_1)*(x4-sec_wheels_x1_1)/(sec_wheels_x2_1-sec_wheels_x1_1)+sec_wheels_y1_1;
+  eng1_F = ((pow((pow((x4-eng_x_1), 2) + pow((y4-eng_y_1), 2)), 0.5)) / L);// * sign(stic_y - ((axis_y2_1-axis_y1_1)*(((axis_x2_1-axis_x1_1)*(axis_y2_1-axis_y1_1)*(stic_y-axis_y1_1)+axis_x1_1*pow(axis_y2_1-axis_y1_1, 2)+stic_x*pow(axis_x2_1-axis_x1_1, 2))/(pow(axis_y2_1-axis_y1_1, 2)+pow(axis_x2_1-axis_x1_1, 2))-axis_x1_1)/(axis_x2_1-axis_x1_1)+axis_y1_1));
 
-  Serial.println(x);
-  Serial.println(y);
+  x5=((sec_wheels_x2_2-sec_wheels_x1_2)*(sec_wheels_y2_2-sec_wheels_y1_2)*(stic_y-sec_wheels_y1_2)+sec_wheels_x1_2*pow(sec_wheels_y2_2-sec_wheels_y1_2, 2)+stic_x*pow(sec_wheels_x2_2-sec_wheels_x1_2, 2))/(pow(sec_wheels_y2_2-sec_wheels_y1_2, 2)+pow(sec_wheels_x2_2-sec_wheels_x1_2, 2));
+  y5=(sec_wheels_y2_2-sec_wheels_y1_2)*(x5-sec_wheels_x1_2)/(sec_wheels_x2_2-sec_wheels_x1_2)+sec_wheels_y1_2;
+  eng2_F = ((pow((pow((x5-eng_x_2), 2) + pow((y5-eng_y_2), 2)), 0.5)) / L) * sign(stic_y - ((axis_y2_2-axis_y1_2)*(((axis_x2_2-axis_x1_2)*(axis_y2_2-axis_y1_2)*(stic_y-axis_y1_2)+axis_x1_2*pow(axis_y2_2-axis_y1_2, 2)+stic_x*pow(axis_x2_2-axis_x1_2, 2))/(pow(axis_y2_2-axis_y1_2, 2)+pow(axis_x2_2-axis_x1_2, 2))-axis_x1_2)/(axis_x2_2-axis_x1_2)+axis_y1_2));
+
+  x6=((sec_wheels_x2_3-sec_wheels_x1_3)*(sec_wheels_y2_3-sec_wheels_y1_3)*(stic_y-sec_wheels_y1_3)+sec_wheels_x1_3*pow(sec_wheels_y2_3-sec_wheels_y1_3, 2)+stic_x*pow(sec_wheels_x2_3-sec_wheels_x1_3, 2))/(pow(sec_wheels_y2_3-sec_wheels_y1_3, 2)+pow(sec_wheels_x2_3-sec_wheels_x1_3, 2));
+  y6=(sec_wheels_y2_3-sec_wheels_y1_3)*(x6-sec_wheels_x1_3)/(sec_wheels_x2_3-sec_wheels_x1_3)+sec_wheels_y1_3;
+  eng3_F = ((pow((pow((x6-eng_x_2), 2) + pow((y6-eng_y_2), 2)), 0.5)) / L) * sign(stic_x - (axis_x2_2-axis_x1_2)*(axis_y2_2-axis_y1_2)*(stic_y-axis_y1_2)+axis_x1_2*pow(axis_y2_2-axis_y1_2, 2)+stic_x*pow(axis_x2_2-axis_x1_2, 2))/(pow(axis_y2_2-axis_y1_2, 2)+pow(axis_x2_2-axis_x1_2, 2));
+
+  Serial.println(stic_x);
+  Serial.println(stic_y);
+  /*Serial.println(x6);
+  Serial.println(y6);*/
+  Serial.println(eng1_F);
 }
